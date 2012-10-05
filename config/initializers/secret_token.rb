da610afb670d45b3a1474087a63d49bd04cf93c7
Alex Moore <alexandermoore@gmail.com>
2012-10-05 11:06:37 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ItaBite::Application.config.secret_token = '2f22caec728d385fbacf198b322916c5ec7e9a74407f51d51b869c3b5b244bcc7f2fc4b26cca51aa328a1acb471cadd8c279648bfc12fec987c9a5a54d8a9e7c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ItaBite::Application.config.session_store :cookie_store, key: '_ita_bite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ItaBite::Application.config.session_store :active_record_store
