f486ab88156ae1e41cb73c2faad2d6892dff3df1
Alex Moore <alexandermoore@gmail.com>
2012-10-19 10:00:30 +0900

    added collapsible table

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -6,6 +6,9 @@ $(function () {
   $('#jobs_search').submit(function () {  
   	$.get(this.action, $(this).serialize(), null, 'script');  
     return false;  
-  });  
+  });
+  $('.hide_items').click(function(){
+  		$(this).closest("tr").next().toggleClass("hidden");
+  })  
 });  
 
