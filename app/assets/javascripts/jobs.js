346e2f3654ba2d497c564a70787c675395611380
Alex Moore <alexandermoore@gmail.com>
2012-11-15 17:11:52 +0900

    Updated Javascript

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,14 +1,5 @@
-$(function () {  
-  $('#jobs th a, #jobs .pagination a').live('click', function () {  
-    $.getScript(this.href);  
-    return false;  
-  })
-  $('#jobs_search').submit(function () {  
-  	$.get(this.action, $(this).serialize(), null, 'script');  
-    return false;  
-  });
-  $('.hide_items').live('click',function(){
-  		$(this).closest("tr").next().toggleClass("hidden");
-  })  
-});  
-
+$(document).ready(function(){
+	$(".hide_items").click(function(){
+		$(this).closest("tr").next().toggleClass("hidden");
+	});
+});
